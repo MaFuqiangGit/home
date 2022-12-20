75e2df7142f11128624d4f5e3db810f3eaee5cc6
马富强 <652299482@qq.com>
2022-12-20 07:40:55 +0000

    0.2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
-window.onload = () => {
-  hljs.highlightAll();
-};
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?aed0fda4e0e63a65c697a5a94fd061ca";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
